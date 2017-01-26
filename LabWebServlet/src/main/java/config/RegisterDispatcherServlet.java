e48c20f90cca5bab56d5ca83fba8fe30dbc05940
陳奕儒 <jack12777@EEIT9117>
2017-01-26 11:51:48 +0800

    Initial Commit

---
+++ LabWebServlet/src/main/java/config/RegisterDispatcherServlet.java
@@ -0,0 +1,20 @@
+package config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class RegisterDispatcherServlet extends AbstractAnnotationConfigDispatcherServletInitializer {
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		int a=1;
+		return null;
+	}
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {SpringMvcJavaConfig.class};
+	}
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"*.controller"};
+	}
+}
+
